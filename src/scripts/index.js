7dc922172dd70ec435df81931f1295741d57e171
Player Nguyên <daudaua3782@gmail.com>
2023-02-20 09:14:10 +0700

    fix: using hash router

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { store } from "../stores/Store";
 import App from "./App";
 
 createRoot(document.getElementById("app")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
